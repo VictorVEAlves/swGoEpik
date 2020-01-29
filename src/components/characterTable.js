09b9f9c6ceadb5776ffca3c17ef55d06fb69abf7
VictorVEAlves <victoredu1@hotmail.com>
2020-01-29 02:07:31 -0300

    ajustes

--- src/components/characterTable.js
+++ src/components/characterTable.js
@@ -2,7 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchCharacters, fetchSpecies } from "../store/characterActions";
 import MaterialTable from 'material-table';
-import {CharacterModal} from './modal.js';
+import { CharacterModal } from './modal.js';
+import { Spinner, Row, Col, Container } from 'react-bootstrap';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -13,7 +14,7 @@ class CharacterTable extends React.Component {
   }
 
   getCharacterById(data) {
-    this.props.dispatch(fetchSpecies(data.species)).then(specie => this.setState({specie: specie}));    
+    this.props.dispatch(fetchSpecies(data.species)).then(specie => this.setState({ specie: specie }));
     this.setState({
       showModal: true,
       character: data,
@@ -21,12 +22,13 @@ class CharacterTable extends React.Component {
   }
 
   render() {
-    let closeModal = () => this.setState({showModal: false});
+    let closeModal = () => this.setState({ showModal: false });
     const { error, loading, characters } = this.props;
     const columns = [
       { title: "Nome", field: "name" },
       { title: "Gênero", field: "gender" },
-      { title: "Ano de nascimento", field: "birth_year" }
+      { title: "Ano de nascimento", field: "birth_year" },
+      { title: "Qtd Filmes", field: "films", render: rowData => rowData.films.length }
     ];
 
     if (error) {
@@ -34,7 +36,15 @@ class CharacterTable extends React.Component {
     }
 
     if (loading) {
-      return <div>Carregando...</div>;
+      return <Container>
+              <Row className="text-center mt-5">
+                <Col>      
+                  <Spinner animation="border" role="status">
+                      <span className="sr-only">Loading...</span>
+                  </Spinner>
+                </Col>
+              </Row>
+            </Container>
     }
 
     return (
@@ -46,20 +56,20 @@ class CharacterTable extends React.Component {
           options={{
             pageSize: 10
           }}
-          
+
           actions={[{
-              icon: 'edit',
-              tooltip: 'Visualizar',
-              onClick: (event, rowData) => this.getCharacterById(rowData),
-            }
+            icon: 'edit',
+            tooltip: 'Visualizar',
+            onClick: (event, rowData) => this.getCharacterById(rowData),
+          }
           ]}
         />
-      <CharacterModal 
-        show={this.state ? this.state.showModal : false}
-        onHide={closeModal}
-        character = {this.state ? this.state.character : null}
-        specie = {this.state ? this.state.specie : null}
-      />
+        <CharacterModal
+          show={this.state ? this.state.showModal : false}
+          onHide={closeModal}
+          character={this.state ? this.state.character : null}
+          specie={this.state ? this.state.specie : null}
+        />
       </React.Fragment>
     );
   }
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import styled from "styled-components";
+import logo from '../img/logo.jpg';
 
 export class CharacterModal extends React.Component {
     render() {
@@ -12,21 +14,42 @@ export class CharacterModal extends React.Component {
             >
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">
-                        {this.props.character ? this.props.character.name : 'Personagem'}
+                        <Title>{this.props.character ? this.props.character.name : 'N/A'}</Title>
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <div>
-                        <p>Espécie: {this.props.specie ? this.props.specie.name : 'n/a'}</p>
-                        <p>Linguagem: {this.props.specie ? this.props.specie.language : 'n/a'}</p>
-                        <p>Veículos:</p>
+                    
+                    <div className="logo text-center">
+                        <img src={logo} className="text-center" width="250" height="150" />
                     </div>
-                     
+                    
+                    <div>
+                        <h3>Espécie: {this.props.specie ? this.props.specie.name : 'n/a'}</h3>
+                        <h3>Linguagem: {this.props.specie ? this.props.specie.language : 'n/a'}</h3>
+                        <h3>Ano de nascimento: {this.props.character ? this.props.character.birth_year : 'n/a'}</h3>
+                    </div>
+
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={this.props.onHide}>Fechar</Button>
+                    <MeuBotao className="btn-fechar" onClick={this.props.onHide}>Fechar</MeuBotao>
                 </Modal.Footer>
             </Modal>
         );
     }
 }
+
+const MeuBotao = styled.button`
+    background: linear-gradient(90deg, rgba(157,48,20,1) 0%, rgba(114,33,32,1) 35%, rgba(197,168,140,1) 100%);
+    border: none;
+    border-radius: 3px;
+    color: #FDFDFD;
+    font-size: 1.2rem;
+    margin: 1rem;
+    padding: .4rem 1.5rem;
+`;
+
+const Title = styled.h1`
+    background: linear-gradient(90deg, rgba(157,48,20,1) 0%, rgba(114,33,32,1) 35%, rgba(197,168,140,1) 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+`;
